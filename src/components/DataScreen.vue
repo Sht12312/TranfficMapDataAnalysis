f792dd3cb52de10b386e23c822784edc05d1272a
Sht12312 <3114911684@qq.com>
2024-10-09 12:48:26 +0800

    Ver0.3

--- src/components/DataScreen.vue
+++ src/components/DataScreen.vue
@@ -1,18 +1,81 @@
 <template>
-    <div>
-        
+    <div class="container">
+      <div class="column left">
+        <div class="box">左上</div>
+        <div class="box">左中</div>
+        <div class="box">左下</div>
+      </div>
+      <div class="column center">
+        <div class="box large">中</div>
+        <div class="box small">中下</div>
+      </div>
+      <div class="column right">
+        <div class="box">右上</div>
+        <div class="box">右中</div>
+        <div class="box">右下</div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  export default {
+    name: "DataScreen",
+    setup() {},
+  };
+  </script>
+  
+  <style scoped>
 
-<script>
-export default {
-    name : 'DataScreen',
-    setup(){
+  .container {display: flex;background-image: url('@/assets/gif/background.gif');background-size: cover;}
+  
+  .column {flex: 1;display: flex;flex-direction: column;}
+  
+  .left .box,
+  .right .box {flex: 1;}
+  
+  .center .large {flex: 2;}
+  
+  .center .small {flex: 1;}
+  
+  .dialog {
+            position: relative;
+            font-size: 20px;
+            text-shadow: 0 10px 12px #001025;
+            width: 300px;
+            height: 350px;
+        }
 
-    }
-}
-</script>
+  .box {
+    margin: 5px;
+    border-radius: 10px;
+    padding-top: 12%;
+    background: linear-gradient(to left, #2CD5FF, #2CD5FF) left top no-repeat,
+    linear-gradient(to bottom, #2CD5FF, #2CD5FF) left top no-repeat,
+    linear-gradient(to left, #2CD5FF, #2CD5FF) right top no-repeat,
+    linear-gradient(to bottom, #2CD5FF, #2CD5FF) right top no-repeat,
+    linear-gradient(to left, #2CD5FF, #2CD5FF) left bottom no-repeat,
+    linear-gradient(to bottom, #2CD5FF, #2CD5FF) left bottom no-repeat,
+    linear-gradient(to left, #2CD5FF, #2CD5FF) right bottom no-repeat,
+    linear-gradient(to left, #2CD5FF, #2CD5FF) right bottom no-repeat;
+    background-size: 4px 20px, 20px 4px, 4px 20px, 20px 4px;
+    border: 1px solid #073F97;
+    position: relative;
+            }
+        .box span {
+            display: inline-block;
+            width: 35%;
+            background:#05286C;
+            font-size: 14px;
+            position: absolute;
+            top: 0;
+            left: 32%;
+            padding: 4px 0px;
+            color: #19E8FE;
+            border-radius: 0 0 10px 10px;
 
-<style>
+        } 
 
-</style>
+
+
+  </style>
+  
---
+++ src/components/TheBorder.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="dialog">
+      <span class="dialog-rect"></span>
+      <div class="box flex-row j_c">CSS 实现不规则边框（2）</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "IrregularBorderBox",
+  };
+  </script>
+  
+  <style scoped>
+  :root {
+    --w: 353px; /* 宽度 */
+    --h: 60px;  /* 高度 */
+    --path: polygon(
+      10px 0,
+      100% 0,
+      100% calc(100% - 10px),
+      calc(100% - 10px) 100%,
+      0 100%,
+      0 10px
+    ); /* clip-path裁剪形状 */
+  }
+  
+  .dialog {
+    position: relative;
+  }
+  
+  .box {
+    position: relative;
+    width: var(--w);
+    height: var(--h);
+    border-radius: 6px;
+    background: linear-gradient(
+      140deg,
+      rgba(138, 203, 255, 0.55),
+      rgba(41, 106, 143, 0.5),
+      rgba(79, 170, 221, 1)
+    );
+    clip-path: var(--path); /* 进行裁剪 */
+    font-size: 20px;
+    color: #fff;
+    margin-bottom: 20px;
+    cursor: pointer;
+    transition: all 0.5s;
+  }
+  
+  .box:last-child {
+    margin-bottom: 0;
+  }
+  
+  .box:hover {
+    filter: brightness(1.5);
+  }
+  
+  .box::after {
+    content: "";
+    width: calc(100% - 3px);
+    height: calc(100% - 3px);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: -1;
+    background: linear-gradient(180deg, #023863 0%, #002d48 100%);
+    border-radius: 4px;
+    clip-path: var(--path); /* 设置裁剪 */
+  }
+  
+  .flex-row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  
+  .j_c {
+    justify-content: center;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  
+  body {
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #222;
+    color: #fff;
+    overflow: hidden;
+    height: 100vh;
+  }
+  </style>
+  
