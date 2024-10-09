7041ac720f5be606729c094be79024f316ecf7b6
Sht12312 <3114911684@qq.com>
2024-10-09 14:42:18 +0800

    ver0.3

--- src/components/DataScreen.vue
+++ src/components/DataScreen.vue
@@ -6,7 +6,9 @@
         <div class="box"></div>
       </div>
       <div class="column center">
-        <div class="box large" id="ChinaMap"></div>
+        <div class="box large" id="ChinaMap">
+          <!-- <div id="ChinaMap"></div> -->
+        </div>
         <div class="box small"></div>
       </div>
       <div class="column right">
@@ -93,117 +95,29 @@
         };
         echarts.registerMap("china", zhongguo);
         ChinaMap.setOption(option);
-  
-        
-      };
-  
-      const initCanvas = () => {
-        const canvas = document.createElement('canvas');
-        canvas.id = "mycanvas";
-        canvas.style.position = "absolute";
-        canvas.style.zIndex = "-2";
-        canvas.style.backgroundColor = "black";
-        document.body.prepend(canvas);
-        
-        let w = window.innerWidth;
-        let h = window.innerHeight;
-        canvas.width = w;
-        canvas.height = h;
-  
-        const ctx = canvas.getContext('2d');
-  
-        function Build() {
-          this.ctx = ctx;
-          this.counts = 3000;
-          this.maxSize = 4;
-          this.halfWidth = w / 2;
-          this.halfHeight = h / 2;
-          this.arr = [];
-        }
-        Build.prototype.add = function(coor) {
-          var grd = this.ctx.createRadialGradient(coor.x, coor.y, coor.size / 2, coor.x, coor.y, coor.size);
-          grd.addColorStop(0, "white");
-          grd.addColorStop(1, coor.color);
-          this.ctx.fillStyle = grd;
-          this.ctx.beginPath();
-          this.ctx.arc(coor.x, coor.y, coor.size, 0, Math.PI * 2, true);
-          this.ctx.transform(1, 0, 0, 1, 0, coor.z);
-          this.ctx.closePath();
-          this.ctx.fill();
-        };
-        Build.prototype.init = function() {
-          this.run();
-          this.render();
-          this.animate();
-        };
-        Build.prototype.run = function() {
-          let nums = 0;
-          while (nums < this.counts) {
-            const coor = {
-              x: Math.ceil(Math.random() * w),
-              y: Math.ceil(Math.random() * h),
-              posx: Math.random() * w - this.halfWidth,
-              posy: Math.random() * h - this.halfHeight,
-              fl: 100,
-              speed: Math.random() * 2,
-              posz: Math.random() * 250,
-              r: Math.ceil(Math.random() * this.maxSize),
-              color: `rgba(${Math.ceil(Math.random() * 255)},${Math.ceil(Math.random() * 255)},${Math.ceil(Math.random() * 255)},${Math.random()})`
-            };
-            this.arr.push(coor);
-            nums++;
-          }
-        };
-        Build.prototype.clear = function() {
-          ctx.clearRect(0, 0, w, h);
-        };
-        Build.prototype.render = function() {
-          this.clear();
-          for (const item of this.arr) {
-            this.draw(item);
-          }
-        };
-        Build.prototype.animate = function() {
-          this.render();
-          requestAnimationFrame(() => this.animate());
-        };
-        Build.prototype.draw = function(item) {
-          if (item.posz > -item.fl) {
-            const scale = item.fl / (item.fl + item.posz);
-            item.x = this.halfWidth + item.posx * scale;
-            item.y = this.halfHeight + item.posy * scale;
-            item.size = item.r * scale;
-            item.posz -= item.speed;
-          } else {
-            item.posz = Math.random() * 250;
-          }
-          this.add(item);
-        };
-  
-        const app = new Build();
-        app.init();
-  
+
         window.addEventListener('resize', () => {
-          canvas.width = w = window.innerWidth;
-          canvas.height = h = window.innerHeight;
+          ChinaMap.resize();
         });
       };
   
-      onMounted(() => {
-        initCanvas();
+      onMounted(async () => {
+        const ChinaDom = document.getElementById('ChinaMap');
+        console.log(ChinaDom);
         initCharts();
       });
-    }
+    },
   };
   </script>
   
   <style scoped>
   .container {
     display: flex;
-    position: relative; /* Ensure content is above canvas */
 
+        background-color: #000e29;
     background-size: cover;
   }
+  
   .left {
     flex: 2;
   }
@@ -213,28 +127,42 @@
   .center {
     flex: 3;
   }
+
   .column {
+
     display: flex;
     flex-direction: column;
   }
-  .left .box, .right .box {
+  
+  .left .box,
+  .right .box {
     flex: 1;
   }
+  
   .center .large {
     flex: 2;
     display: flex;
-    background-size: cover;
     justify-content: center;
     align-items: center;
     position: relative;
   }
+  
   .center .small {
     flex: 1;
   }
+  
   .box {
     margin: 5px;
     border-radius: 10px;
-    padding: 0;
+    padding: 0; 
+    background: linear-gradient(to left, #2CD5FF, #2CD5FF) left top no-repeat,
+                linear-gradient(to bottom, #2CD5FF, #2CD5FF) left top no-repeat,
+                linear-gradient(to left, #2CD5FF, #2CD5FF) right top no-repeat,
+                linear-gradient(to bottom, #2CD5FF, #2CD5FF) right top no-repeat,
+                linear-gradient(to left, #2CD5FF, #2CD5FF) left bottom no-repeat,
+                linear-gradient(to bottom, #2CD5FF, #2CD5FF) left bottom no-repeat,
+                linear-gradient(to left, #2CD5FF, #2CD5FF) right bottom no-repeat,
+                linear-gradient(to left, #2CD5FF, #2CD5FF) right bottom no-repeat;
     background-size: 4px 20px, 20px 4px, 4px 20px, 20px 4px;
     border: 1px solid #073F97;
     position: relative;
